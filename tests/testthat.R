968ffe643cbb5918de5bf05853e6fbf4a402f336
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-11-07 14:06:21 -0800

    refactor repo

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(voe)
-
-test_check("voe")
--- tests/testthat/test-compute_metaanalysis.R
+++
@@ -1,9 +0,0 @@
-test_that("meta-analysis works", {
-	association_output = readRDS('sample_association_output.rds')
-	association_output2 = association_output
-	association_output2$dsid=2
-	association_output_doubled = dplyr::bind_rows(association_output,association_output2)
-	metaanalysis = compute_metaanalysis(association_output_doubled)
-	metaanalysis_cleaned <- clean_metaanalysis(metaanalysis,dataset_num=2)
-  	expect_equal(nrow(metaanalysis_cleaned),79, regexp = NA)
-})
