e56bbba798af6998c27fd83ee15b15a311d3940f
Braden Tierney <bradentierney@Bradens-MacBook-Pro.local>
2020-11-07 15:16:17 -0800

    r command check compliance

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(voe)
+library(quantvoe)
 
-test_check("voe")
+test_check("quantvoe")
--- tests/testthat/test-independent_variable_analysis.R
+++
@@ -1,8 +0,0 @@
-test_that("metadata analysis works", {
-  metadata <- readRDS('metadata_for_test.rds')
-  metadata <- metadata[2:length(metadata)]
-  testthat::expect_error(ind_var_analysis(metadata), regexp = NA)
-  system('rm -rf Metadata_Files')
-  ind_var_analysis(metadata)
-  system('rm -rf Metadata_Files')
-})
